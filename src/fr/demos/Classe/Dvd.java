545a4757ca4aca25222057345282b1b53b42d1d3
unknown <Stagiaire@R203-10.demostraining.com>
2016-04-12 10:43:54 +0200

    génération de la classe DVD

--- src/fr/demos/Classe/Dvd.java
+++ src/fr/demos/Classe/Dvd.java
@@ -1,9 +1,63 @@
 package fr.demos.Classe;
+import fr.demos.Classe.Article;
 
-public class Dvd {
+public class Dvd extends Article {
+	private String titre;
+	private String auteur;
+	private String editeur;
+	private String genre;
+	private int nbrDvd;
 
-	public Dvd() {
-		String dvd;
+	public Dvd(int prixHt, int stock, String description, int reference,String titre, String auteur, String editeur, String genre) {
+		super(prixHt, stock, description, reference);
+		this.titre=titre;
+		this.auteur=auteur;
+		this.editeur=editeur;
+		this.genre=genre;
+	}
+	///Affichage
+@Override
+	public String toString() {
+		return super.toString() + titre + ", auteur=" + auteur + ", editeur=" + editeur + ", genre=" + genre + "]";
+	}
+	///// getter setter
+
+
+	public String getTitre() {
+		return titre;
+	}	
+	public int getNbrDvd() {
+		return nbrDvd;
+	}
+	public void setNbrDvd(int nbrDvd) {
+		this.nbrDvd = nbrDvd;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getEditeur() {
+		return editeur;
+	}
+
+	public void setEditeur(String editeur) {
+		this.editeur = editeur;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
 	}
 
 }
