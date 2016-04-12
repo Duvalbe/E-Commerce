3e18ee392b33008c006c39d5cf16241b7008ecd5
unknown <Stagiaire@R203-10.demostraining.com>
2016-04-12 12:28:45 +0200

    Mise à jour de la classe DVD

--- src/fr/demos/Classe/Dvd.java
+++ src/fr/demos/Classe/Dvd.java
@@ -4,21 +4,21 @@ import fr.demos.Classe.Article;
 public class Dvd extends Article {
 	private String titre;
 	private String auteur;
-	private String editeur;
+	
 	private String genre;
 	private int nbrDvd;
 
-	public Dvd(int prixHt, int stock, String description, int reference,String titre, String auteur, String editeur, String genre) {
+	public Dvd(int prixHt, int stock, String description, int reference,String titre, String auteur,  String genre) {
 		super(prixHt, stock, description, reference);
 		this.titre=titre;
 		this.auteur=auteur;
-		this.editeur=editeur;
+		
 		this.genre=genre;
 	}
 	///Affichage
 @Override
 	public String toString() {
-		return super.toString() + titre + ", auteur=" + auteur + ", editeur=" + editeur + ", genre=" + genre + "]";
+		return super.toString() + titre + ", auteur=" + auteur +  ", genre=" + genre + "]";
 	}
 	///// getter setter
 
@@ -44,13 +44,7 @@ public class Dvd extends Article {
 		this.auteur = auteur;
 	}
 
-	public String getEditeur() {
-		return editeur;
-	}
-
-	public void setEditeur(String editeur) {
-		this.editeur = editeur;
-	}
+	
 
 	public String getGenre() {
 		return genre;
